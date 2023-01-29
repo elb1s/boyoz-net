619ac635624738c33ed0ca1c6996a5fc36d05f5d
elb1s <77449139+elb1s@users.noreply.github.com>
2023-01-29 23:36:11 +0300

    Update Config.js

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -5,11 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "buartik-a2321.firebaseapp.com",
-  projectId: "buartik-a2321",
-  storageBucket: "buartik-a2321.appspot.com",
-  messagingSenderId: "669190119648",
-  appId: "1:669190119648:web:f0bd15134ee87a61ff3085",
+  authDomain: process.env.REACT_aPP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
